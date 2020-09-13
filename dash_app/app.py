b5159693a800b439ec0d489d32bd81d66086d0e1
paulina.knut <paulina.knut@codilime.com>
2020-09-13 14:10:12 +0200

    Configure server

--- dash_app/app.py
+++ dash_app/app.py
@@ -12,40 +12,42 @@ from .callbacks import poagraph
 from .callbacks import pangtreebuild
 from .callbacks import visualisation
 
-app.title = 'PangtreeVis'
-app.css.config.serve_locally = False
-app.scripts.config.serve_locally = True
-
-external_css = [
-    'https://use.fontawesome.com/releases/v5.8.1/css/all.css',
-    # dbc.themes.FLATLY
-]
-for css in external_css:
-    app.css.append_css({"external_url": css})
 
 def get_nav_link(fa_icon, span, href, id):
-    return html.Li(html.A(
-        [
-            html.I(className=f"fas fas-nav {fa_icon}"), 
-            html.Span(span, className="nav-text")
-        ],
-        href=href, 
-        id=id), className="has-subnav high")
+    return html.Li(
+        html.A(
+            [
+                html.I(className=f"fas fas-nav {fa_icon}"), 
+                html.Span(span, className="nav-text")
+            ],
+            href=href, 
+            id=id
+        ), 
+        className="has-subnav high"
+    )
 
 
-app.layout = html.Div([
-    dcc.Location(id="url", refresh=False),
-    html.Div([], className="area"),
-    dbc.Navbar([
-        html.Ul([
-            get_nav_link("fa-home", "Home", "/#", "index_nav"),
-            get_nav_link("fa-seedling", "PangTreeBuild", "/pangtreebuild", "pangbuild_nav"),
-            get_nav_link("fa-tree", "PangTreeVis", "/pangtreevis", "pangtreevis_nav"),
-            get_nav_link("fa-question-circle", "FAQ", "/faq", "faq_nav"),
-        ])
-    ], className="main-menu", sticky="left"),
-    html.Div(id="page_content", style={'margin-left': '60px'})
-])
+app.layout = html.Div(
+    [
+        dcc.Location(id="url", refresh=False),
+        html.Div([], className="area"),
+        dbc.Navbar(
+            [
+                html.Ul(
+                    [
+                        get_nav_link("fa-home", "Home", "/#", "index_nav"),
+                        get_nav_link("fa-seedling", "PangTreeBuild", "/pangtreebuild", "pangbuild_nav"),
+                        get_nav_link("fa-tree", "PangTreeVis", "/pangtreevis", "pangtreevis_nav"),
+                        get_nav_link("fa-question-circle", "FAQ", "/faq", "faq_nav"),
+                    ]
+                )
+            ], 
+            className="main-menu", 
+            sticky="left"
+        ),
+        html.Div(id="page_content", style={'margin-left': '60px'})
+    ]
+)
 
 
 @app.callback([Output("page_content", 'children'),
